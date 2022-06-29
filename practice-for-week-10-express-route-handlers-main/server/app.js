94be99b32ca70dde1f88aec3bec46423ecc52830
Megha Sahgal <meghasahgal@Meghas-MacBook-Air.local>
2022-06-28 20:27:03 -0700

    1

--- practice-for-week-10-express-route-handlers-main/server/app.js
+++ practice-for-week-10-express-route-handlers-main/server/app.js
@@ -37,5 +37,44 @@ app.get('/artists', (req, res, next) => {
   res.json(getAllArtists());
 });
 
+app.post('/artists', (req, res, next) =>{
+  res.status(201)
+  res.json(addArtist(req.body))
+})
+//{
+//     "name": "Led Zeppelin",
+//     "artistId": 2
+// }
+
+app.get('/artists/latest', (req, res, next)=>{
+  res.status(200)
+  res.json(getLatestArtist())
+})
+
+// {
+//     "latest": {
+//         "artistId": 1,
+//         "name": "Red Hot Chili Peppers"
+//     }
+// }
+
+
+app.get('/artists/latest/albums', (req, res, next)=>{
+  res.status(200)
+  res.json(getAlbumsForLatestArtist())
+})
+
+// {
+//     "latest": {
+//         "albums": [
+//             {
+//                 "albumId": 1,
+//                 "name": "Stadium Arcadium",
+//                 "artistId": 1
+//             }
+//         ]
+//     }
+// }
+
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
