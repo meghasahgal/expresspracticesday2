947dbd50dfb68620495a5576172f1cf71574dc16
aoavzm <aoavzm@gmail.com>
2022-06-28 20:44:08 -0700

    2

--- practice-for-week-10-express-route-order-main/server/app.js
+++ practice-for-week-10-express-route-order-main/server/app.js
@@ -8,7 +8,11 @@ app.get('/', (req, res) => {
 app.get('/hello', (req, res) => {
     res.send("Hello, my friend!");
 });
-git 
+
+app.get('/goodbye/until/forever', (req, res) => {
+    res.send("So long. Farewell. Have a great life!");
+});
+
 app.get('/goodbye/until/:time', (req, res) => {
     res.send(`Goodbye. See you ${req.params.time}.`);
 });
@@ -20,9 +24,7 @@ app.get(['/goodbye', '/goodbye/*'], (req, res, next) => {
 
 
 
-app.get('/goodbye/until/forever', (req, res) => {
-    res.send("So long. Farewell. Have a great life!");
-});
+
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
